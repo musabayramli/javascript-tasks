c5b9afa1c0633b549d4fff68e025e46e83d79e25
Musa BAyramli <musabayramli2020@gmail.com>
2024-09-07 13:03:56 +0400

    update code

--- search_letter/index.js
+++ search_letter/index.js
@@ -6,15 +6,18 @@ let a = demo.innerHTML;
 
 button.addEventListener("click", function () {
   let searchValue = inpValue.value.trim();
-  let count = (a.match(new RegExp(searchValue, "gi")) || []).length;
+  let lowerCaseSearchValue = searchValue.toLowerCase();
+  let count = (a.toLowerCase().match(new RegExp(lowerCaseSearchValue, "gi")) || []).length;
 
   if (searchValue && count > 0) {
     error.innerHTML = `Axtardığınız netice: ${searchValue}<br/> Təkrarlanma sayı: ${count}`;
     demo.innerHTML = a;
-    demo.innerHTML = demo.innerHTML.replaceAll(
-      `${searchValue}`,
-      `<span>${searchValue}</span>`
-    );
+
+    
+    demo.innerHTML = demo.innerHTML.replace(new RegExp(`(${searchValue})`, "gi"), function (match) {
+      return `<span>${match}</span>`; 
+    });
+
     inpValue.value = "";
   } else if (!searchValue) {
     error.innerHTML = "Input-a bir şeylər yaz !!!";
